811a3eb34abf2a17ac4c376a0082d061a39d072f
jwzhang.ece@gmail.com <jwzhang.ece@gmail.com@72ff391e-639b-8411-1f25-85f9999cf611>
2012-09-22 22:37:11 +0000

    use asyn to post wish text and photo to avoid crash, added authorization error report, added several listenner class

--- FancyToDoList/src/com/wish/wishlist/activity/WishItemPostToSNS.java
+++ FancyToDoList/src/com/wish/wishlist/activity/WishItemPostToSNS.java
@@ -50,13 +50,13 @@ public class WishItemPostToSNS extends Activity {
 	private AsyncFacebookRunner _asyncRunner;
 	private long _itemId;
 	private Context _ctx;
+	private String _message;
+	private byte[] _photoData;
 
 	public static final int LOGIN = Menu.FIRST;
 	public static final int GET_EVENTS = Menu.FIRST + 1;
 	public static final int GET_ID = Menu.FIRST + 2;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		_ctx = this;
 		Bundle extras = getIntent().getExtras();
@@ -73,39 +73,48 @@ public class WishItemPostToSNS extends Activity {
 			@Override
 			public void onFacebookError(FacebookError e) {
 				Log.d("FACEBOOK ERROR","FB ERROR. MSG: "+e.getMessage()+", CAUSE: "+e.getCause());
+				((Activity)_ctx).runOnUiThread(new Runnable() {
+					public void run() {
+						Toast.makeText(_ctx, "FACEBOOK ERROR", Toast.LENGTH_SHORT).show();
+					}
+				});
 			}
 
 			@Override
 			public void onError(DialogError e) {
 				Log.e("ERROR","AUTH ERROR. MSG: "+e.getMessage()+", CAUSE: "+e.getCause());
+				((Activity)_ctx).runOnUiThread(new Runnable() {
+					public void run() {
+						Toast.makeText(_ctx, "AUTHORIZATION ERROR", Toast.LENGTH_SHORT).show();
+					}
+				});
 			}
 
 			@Override
 			public void onCancel() {
 				Log.d("CANCELLED","AUTH CANCELLED");
+				((Activity)_ctx).runOnUiThread(new Runnable() {
+					public void run() {
+						Toast.makeText(_ctx, "AUTHORIZATION CANCELLED", Toast.LENGTH_SHORT).show();
+					}
+				});
 			}
 		});
-		//_asyncRunner = new AsyncFacebookRunner(_facebook);
 	}
 
 	public void postWishToWall(String accessToken) {
-		//we need to put this outside the ui thread, otherwise, a runtime execption will occur.
-		//new Thread() {
-		//	public void run() {
-					Log.d("JSON", "run try {");
-					WishItem wish_item = WishItemManager.getInstance(_ctx).retrieveItembyId(_itemId);
-					String message = wish_item.getShareMessage(true);
-					byte[] photoData = wish_item.getPhotoData();
-					
-					if (photoData == null) {
-						postTextWish(message);
-					}
-					else {
-						postTextAndPhotoWish(message, photoData);
-					}
-			}//end of run
-		//}.start();
-	//}
+		Log.d("JSON", "run try {");
+		WishItem wish_item = WishItemManager.getInstance(_ctx).retrieveItembyId(_itemId);
+		_message = wish_item.getShareMessage(true);
+		_photoData = wish_item.getPhotoData();
+		
+		if (_photoData == null) {
+			postTextWish(_message);
+		}
+		else {
+			postTextAndPhotoWish();
+		}
+	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -117,19 +126,15 @@ public class WishItemPostToSNS extends Activity {
 
 	private void postTextWish(String message) {
 		Log.d(TAG, "postTextWish");
-		//String response = null;
 		Bundle params = new Bundle();
-		//try {
-			//params.putString("message", message);
-			params.putString("method", "status.set");
-			params.putString("status", message);
-			//response = _facebook.request("me/feed", params, "POST");
-			_asyncRunner.request(params, new simpleRequestListener());
-		//	Log.d(TAG, "response update status:\n" + response);
-		//}
+		//params.putString("message", message);
+		params.putString("method", "status.set");
+		params.putString("status", message);
+		//response = _facebook.request("me/feed", params, "POST");
+		_asyncRunner.request(params, new postTextRequestListener());
 	}
 
-	private void postTextAndPhotoWish(String message, byte[] photoData) {
+	private void postTextAndPhotoWish() {
 		//it appears that there is not easy way to post a message with photo on wall, and make it to
 		//appear on friends' feed using facebook's existing api
 
@@ -142,82 +147,18 @@ public class WishItemPostToSNS extends Activity {
 		//it assumes user already has posted some photos to his/her wall sometime in the past.
 		//It will fail if there are no wall photos.
 		Log.d(TAG, "postTextAndPhotoWish");
-		String wallAlbumID = null;
-		String response = null;
-		try {
-			response = _facebook.request("me/albums");
-			Log.d(TAG, "response me/albums:\n" + response);
-		}
-		catch (MalformedURLException e) {
-			Log.e("MALFORMED URL",""+e.getMessage());
-		}
-		catch (IOException e) {
-			Log.e("IOEX",""+e.getMessage());
-		}
+		_asyncRunner.request("me/albums", new albumRequestListener());
 
-		try {
-			Log.d("JSON", "JSON run try {");
-			JSONObject json = Util.parseJson(response);
-			JSONArray albums = json.getJSONArray("data");
-			for (int i = 0; i < albums.length(); i++) {
-				Log.d("JSON", "i: " + String.valueOf(i));
-				JSONObject album = albums.getJSONObject(i);
-				if (album.getString("type").equalsIgnoreCase("wall")) {
-					wallAlbumID = album.getString("id");
-					Log.d("JSON", "wallAlbumID" + wallAlbumID);
-					break;
-				}
-			}
-		}
-		catch (JSONException e) {
-			Log.d("JSONException","ERROR. MSG: "+e.getMessage()+", CAUSE: "+e.getCause());
-		//	e.printStackTrace();
-		}
-
-		String requestFlag = "";
-		Bundle params = new Bundle();
-		if (wallAlbumID != null) {
-			Log.d("JSON", "wall album exists");
-			params.putString("message", message);
-			params.putByteArray("source", photoData);
-			requestFlag = wallAlbumID + "/photos";
-		}
-		else { //there is no wall album for this user, meaning the user has never posted any
-			//photo on his/her wall before (a case unlikely), so use "me/photo" request to
-			//upload the photo, this will not automatically create a wall album, instead,
-			//it will create an album named "Beans Wishlist Photos" and upload the photo
-			//to this album. subsequent wish share will upload photos to this album until user 
-			//post their first photo to their wall album from outside this app
-			//these photos will appear in friends feed as a signle album instead of sepearte 
-			//wish post
-			Log.d("me/photos", "no wall album");
-			params.putString("message", message);
-			//bundle.putString("method", "photos.upload");
-			params.putByteArray("picture", photoData);
-			//bundle.putString(Facebook.TOKEN, accessToken);
-			requestFlag = "me/photos";
-		}
-
-		//try {
-			_asyncRunner.request(requestFlag, params, "POST", new simpleRequestListener(), null);
-		//	Log.d(TAG, "response " + requestFlag + "\n" + response);
-		//}
 	}
 	
 	private class simpleRequestListener implements RequestListener {
-		/**
+		/*
 		* Called when a request completes with the given response.
-		*
+		
 		* Executed by a background thread: do not update the UI in this method.
 		*/
 		public void onComplete(String response, Object state) {
 			Log.d(TAG, "response "  + "\n" + response);
-			if (response.equals("true")) {
-				showSucessToast();
-			}
-			else {
-				showFailToast();
-			}
 		}
 		/**
 		* Called when a request has a network or request error.
@@ -254,7 +195,7 @@ public class WishItemPostToSNS extends Activity {
 			showFailToast();
 		}
 		
-		private void showSucessToast() {
+		protected void showSucessToast() {
 			((Activity)_ctx).runOnUiThread(new Runnable() {
 				public void run() {
 					Toast.makeText(_ctx, "Success", Toast.LENGTH_SHORT).show();
@@ -262,12 +203,88 @@ public class WishItemPostToSNS extends Activity {
 			});
 		}
 
-		private void showFailToast() {
+		protected void showFailToast() {
 			((Activity)_ctx).runOnUiThread(new Runnable() {
 				public void run() {
 					Toast.makeText(_ctx, "Fail", Toast.LENGTH_SHORT).show();
 				}
 			});
+		}
+	}
+
+	private class postTextRequestListener extends simpleRequestListener {
+		public void onComplete(String response, Object state) {
+			super.onComplete(response, state);
+			if (response.equals("true")) {
+				showSucessToast();
+			}
+			else {
+				showFailToast();
+			}
+		}
+	}
+
+	private class postTextAndPhotoRequestListener extends simpleRequestListener {
+		public void onComplete(String response, Object state) {
+			super.onComplete(response, state);
+			showSucessToast();
+		}
+	}
+
+	private class albumRequestListener extends simpleRequestListener {
+		/**
+		* Called when a request completes with the given response.
+		*
+		* Executed by a background thread: do not update the UI in this method.
+		*/
+		@Override
+		public void onComplete(String response, Object state) {
+			Log.d(TAG, "response "  + "\n" + response);
+			String wallAlbumID = null;
+			try {
+				Log.d("JSON", "JSON run try {");
+				JSONObject json = Util.parseJson(response);
+				JSONArray albums = json.getJSONArray("data");
+				for (int i = 0; i < albums.length(); i++) {
+					Log.d("JSON", "i: " + String.valueOf(i));
+					JSONObject album = albums.getJSONObject(i);
+					if (album.getString("type").equalsIgnoreCase("wall")) {
+						wallAlbumID = album.getString("id");
+						Log.d("JSON", "wallAlbumID" + wallAlbumID);
+						break;
+					}
+				}
+			}
+			catch (JSONException e) {
+				Log.d("JSONException","ERROR. MSG: "+e.getMessage()+", CAUSE: "+e.getCause());
+			//	e.printStackTrace();
+			}
+
+			String requestFlag = "";
+			Bundle params = new Bundle();
+			if (wallAlbumID != null) {//there is a wall album, post the message and photo to the wall album
+				Log.d("JSON", "wall album exists");
+				params.putString("message", _message);
+				params.putByteArray("source", _photoData);
+				requestFlag = wallAlbumID + "/photos";
+			}
+			else { //there is no wall album for this user, meaning the user has never posted any
+				//photo on his/her wall before (a case unlikely), so use "me/photos" request to
+				//upload the photo, this will not automatically create a wall album, instead,
+				//it will create an album named "Beans Wishlist Photos" and upload the photo
+				//to this album. subsequent wish share will upload photos to this album until user 
+				//post their first photo to their wall album from outside this app
+				//these photos will appear in friends feed as a signle album instead of sepearte 
+				//wish post
+				Log.d("me/photos", "no wall album");
+				params.putString("message", _message);
+				//bundle.putString("method", "photos.upload");
+				params.putByteArray("picture", _photoData);
+				//bundle.putString(Facebook.TOKEN, accessToken);
+				requestFlag = "me/photos";
+			}
+
+			_asyncRunner.request(requestFlag, params, "POST", new postTextAndPhotoRequestListener(), null);
 		}
 	}
 }
