86900df3d49d0a2ce39c2e390262c142180e372b
jwzhang.ece@gmail.com <jwzhang.ece@gmail.com@72ff391e-639b-8411-1f25-85f9999cf611>
2013-05-11 03:27:09 +0000

    add preference xml and activity

---
+++ FancyToDoList/src/com/wish/wishlist/activity/WishListPreference.java
@@ -0,0 +1,38 @@
+package com.wish.wishlist.activity;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.widget.Toast;
+
+import com.wish.wishlist.R;
+ 
+public class WishListPreference extends PreferenceActivity {
+	@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.layout.wishlist_preference);
+			// Get the custom preference
+			Preference customPref = (Preference) findPreference("customPref");
+			customPref
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+					public boolean onPreferenceClick(Preference preference) {
+						Toast.makeText(getBaseContext(),
+							"The custom preference has been clicked",
+							Toast.LENGTH_LONG).show();
+						SharedPreferences customSharedPreference = getSharedPreferences(
+							"myCustomSharedPrefs", Activity.MODE_PRIVATE);
+						SharedPreferences.Editor editor = customSharedPreference
+					.edit();
+				editor.putString("myCustomPref",
+					"The preference has been clicked");
+				editor.commit();
+				return true;
+					}
+
+				});
+		}
+}
