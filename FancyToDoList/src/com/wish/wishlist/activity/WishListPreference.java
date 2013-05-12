b33ab51f99f5aa3a84f1c698a3f536da43f3d6de
jwzhang.ece@gmail.com <jwzhang.ece@gmail.com@72ff391e-639b-8411-1f25-85f9999cf611>
2013-05-12 17:38:18 +0000

    update preference to show auto tag location, release notes and rate the app

--- FancyToDoList/src/com/wish/wishlist/activity/WishListPreference.java
+++ FancyToDoList/src/com/wish/wishlist/activity/WishListPreference.java
@@ -16,23 +16,29 @@ public class WishListPreference extends PreferenceActivity {
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.layout.wishlist_preference);
 			// Get the custom preference
-			Preference customPref = (Preference) findPreference("customPref");
-			customPref
-				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-					public boolean onPreferenceClick(Preference preference) {
-						Toast.makeText(getBaseContext(),
-							"The custom preference has been clicked",
-							Toast.LENGTH_LONG).show();
-						SharedPreferences customSharedPreference = getSharedPreferences(
-							"myCustomSharedPrefs", Activity.MODE_PRIVATE);
-						SharedPreferences.Editor editor = customSharedPreference
-					.edit();
-				editor.putString("myCustomPref",
-					"The preference has been clicked");
-				editor.commit();
-				return true;
-					}
+			Preference releaseNotes = (Preference) findPreference("releaseNotes");
+			releaseNotes.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+				public boolean onPreferenceClick(Preference preference) {
+					Toast.makeText(getBaseContext(),
+						"The release notes has been clicked",
+						Toast.LENGTH_LONG).show();
+			//		SharedPreferences customSharedPreference = getSharedPreferences(
+			//			"myCustomSharedPrefs", Activity.MODE_PRIVATE);
+			//		SharedPreferences.Editor editor = customSharedPreference;
+			//	.edit();
+			//editor.putString("myCustomPref",
+			//	"The preference has been clicked");
+			//editor.commit();
+					return true;
+				}
+			});
 
-				});
+			Preference rateApp = (Preference) findPreference("rateApp");
+			rateApp.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+				public boolean onPreferenceClick(Preference preference) {
+					Toast.makeText(getBaseContext(), "The rate app has been clicked", Toast.LENGTH_LONG).show();
+					return true;
+				}
+			});
 		}
 }
