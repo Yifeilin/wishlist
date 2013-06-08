4a5a2e021d06a2fdfb8dc678fb3f18cfbe00c2be
jwzhang.ece@gmail.com <jwzhang.ece@gmail.com@72ff391e-639b-8411-1f25-85f9999cf611>
2013-06-08 06:51:35 +0000

    fix action bar appears briefly in the splash when app launches

--- FancyToDoList/src/com/wish/wishlist/activity/Splash.java
+++ FancyToDoList/src/com/wish/wishlist/activity/Splash.java
@@ -1,9 +1,7 @@
 package com.wish.wishlist.activity;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -14,10 +12,6 @@ public class Splash extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			ActionBar actionBar = getActionBar();
-			actionBar.hide();
-		}
 
 		setContentView(R.layout.splash);
 
